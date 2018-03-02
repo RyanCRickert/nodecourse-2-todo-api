f8c6fbd9ae5b6fe9d842a4f9dd6e9d873f681c2b
Ryan Rickert <RyanCRickert@gmail.com>
2018-03-02 11:22:49 -0600

    Changed user/pass for DB access

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect("mongodb://ryancrickert:Timbob11@ds253918.mlab.com:53918/rcrdatabase1"|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://user1:password1@ds253918.mlab.com:53918/rcrdatabase1"|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
