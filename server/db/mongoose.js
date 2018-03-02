e0e6f761f4cfdf1cc61cd48614108d5ad3f1eada
Ryan Rickert <RyanCRickert@gmail.com>
2018-03-02 11:38:47 -0600

    Changed connection method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect("mongodb://user1:password1@ds253918.mlab.com:53918/rcrdatabase1"|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
